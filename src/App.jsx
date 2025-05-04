ccbce47ea2f8430348a945765492940975cd5f3f
Chinh Nguyen <nguyenchuu@yahoo.com>
2025-05-04 12:29:13 -0500

    feat: integrate Tailwind CSS for styling
    - Added Tailwind CSS and its Vite plugin to dependencies.
    - Updated App component to use Tailwind CSS classes for styling.
    - Imported Tailwind CSS in the global CSS file.
    - Configured Vite to use Tailwind CSS plugin.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <>
       <h2>Hi, my name is</h2>
-      <h1>Chinh Nguyen,</h1>
+      <h1 className="text-3xl font-bold underline">Chinh Nguyen,</h1>
       <h2>I'm a Software Developer</h2>
     </>
   )
