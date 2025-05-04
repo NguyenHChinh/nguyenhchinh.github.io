761449f02e51e1ffe0b22beaffdc508797f1f8c2
Chinh Nguyen <nguyenchuu@yahoo.com>
2025-05-04 12:57:32 -0500

    style: made hero div, intro statement created

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ function App() {
 
   return (
     <>
-      <h2>Hi, my name is</h2>
-      <h1 className="text-3xl font-bold underline">Chinh Nguyen,</h1>
-      <h2>I'm a Software Developer</h2>
+      <div className="hero">
+        <h2 className="text-2xl">Hi, my name is</h2>
+        <h1 className="text-6xl"><span className="bg-gradient-to-r from-blue-400 to-blue-700 bg-clip-text text-transparent">Chinh Nguyen</span>,</h1>
+        <h2 className="text-6xl">I'm a <span className="bg-gradient-to-r from-blue-700 to-blue-400 bg-clip-text text-transparent">Software Developer</span></h2>
+      </div>
     </>
   )
 }
