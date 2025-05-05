783b27dc8df0f1d4f048896b6bec99410f81d98e
Chinh Nguyen <nguyenchuu@yahoo.com>
2025-05-04 20:47:24 -0500

    feat: navBar disappears when scroll past 10rem

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from 'react';
 
 function Navbar() {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [visible, setVisible] = useState(true);
 
     useEffect(() => {
-        
-    }, [isDarkMode]);
-
-    function toggleMode() {
-        setIsDarkMode((prevMode) => !prevMode);
-    }
+        function handleScroll() {
+            const threshold = 10 * 16; // threshold = rem * 16px
+            setVisible(window.scrollY <= threshold);
+        }
+    
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
 
     return (
-        <div className='navbar-container'>
+        <div className={`navbar-container transition-opacity duration-300 ${visible ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
             <nav className='text-2xl sm:text-3xl lg:text-3xl py-4 pr-6 flex justify-end items-center fixed top-5 right-5 z-50 bg-transparent'>
             <div className='flex gap-x-10 font-medium'>
                 {['About', 'Skills', 'Projects', 'Contact'].map((item) => (
